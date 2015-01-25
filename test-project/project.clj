b7108cc84777a4f007b47bb0e7256d30fd957212
Kenji Nakamura <kenji@loyal3.com>
2015-01-25 15:00:11 -0800

    Bumped up to 0.1.1 release.

--- test-project/project.clj
+++ test-project/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-zinc "0.1.1-RC1"]]
+  :plugins [[lein-zinc "0.1.1"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.scala-lang/scala-library "2.11.4"]]
   :prep-tasks ["zinc" "compile"]
