40dbba70240bd090def081ebbb56793a535b1001
Kenji Nakamura <kenji@loyal3.com>
2015-01-25 14:22:21 -0800

    added the sections that required by clojars.

--- project.clj
+++ project.clj
@@ -1,8 +1,14 @@
 (defproject lein-zinc "0.1.1-SNAPSHOT"
   :description "Typesafe zinc scala incremental compiler plugin"
   :url "https://github.com/k2n/lein-zinc"
+  :scm {:name "git"
+        :url "https://github.com/k2n/lein-zinc"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :pom-addition [:developers [:developer
+                                [:name "Kenji Nakamura"]
+                                [:email "kenjin@clazzsoft.com"]
+                                [:timezone "-8"]]]
   :eval-in-leiningen true
   :dependencies [[com.typesafe.zinc/zinc "0.3.5.3"]
                  [leiningen "2.5.1"]
