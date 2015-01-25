4fe1ff8ed421e3949dd2a6b5bbf0665d24e4d8aa
Kenji Nakamura <kenji@loyal3.com>
2015-01-25 15:02:26 -0800

    Bumped up to 0.2.0-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-zinc "0.1.1"
+(defproject lein-zinc "0.2.0-SNAPSHOT"
   :description "Typesafe zinc scala incremental compiler plugin"
   :url "https://github.com/k2n/lein-zinc"
   :scm {:name "git"
